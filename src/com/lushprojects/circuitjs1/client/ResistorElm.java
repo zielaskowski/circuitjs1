28e868a0a141d42884cba7bc9167f3dfb5a36780
zielaskowski <michal.zielaskowski@gmail.com>
2026-09-17 20:26:11 +0200

    formatting for elementName

--- src/com/lushprojects/circuitjs1/client/ResistorElm.java
+++ src/com/lushprojects/circuitjs1/client/ResistorElm.java
@@ -1,6 +1,6 @@
-/*    
+/*
     Copyright (C) Paul Falstad and Iain Sharp
-    
+
     This file is part of CircuitJS1.
 
     CircuitJS1 is free software: you can redistribute it and/or modify
@@ -24,113 +24,139 @@ import com.lushprojects.circuitjs1.client.util.Locale;
 import com.google.gwt.xml.client.Element;
 import com.google.gwt.xml.client.Document;
 
-    class ResistorElm extends CircuitElm {
+class ResistorElm extends CircuitElm {
 	double resistance;
-	public ResistorElm(int xx, int yy) { super(xx, yy); resistance = 1000; }
-	public ResistorElm(int xa, int ya, int xb, int yb, int f,
-		    StringTokenizer st) {
-	    super(xa, ya, xb, yb, f);
-	    resistance = new Double(st.nextToken()).doubleValue();
+
+	public ResistorElm(int xx, int yy) {
+		super(xx, yy);
+		resistance = 1000;
 	}
-	int getDumpType() { return 'r'; }
+
+	public ResistorElm(int xa, int ya, int xb, int yb, int f, StringTokenizer st) {
+		super(xa, ya, xb, yb, f);
+		resistance = new Double(st.nextToken()).doubleValue();
+	}
+
+	int getDumpType() {
+		return 'r';
+	}
 
 	String dump() {
-	    return super.dump() + " " + resistance;
+		return super.dump() + " " + resistance;
 	}
 
 	void dumpXml(Document doc, Element elem) {
-	    super.dumpXml(doc, elem);
-	    XMLSerializer.dumpAttr(elem, "r", resistance);
+		super.dumpXml(doc, elem);
+		XMLSerializer.dumpAttr(elem, "r", resistance);
 	}
 
 	void undumpXml(XMLDeserializer xml) {
-	    super.undumpXml(xml);
-	    resistance = xml.parseDoubleAttr("r", resistance);
+		super.undumpXml(xml);
+		resistance = xml.parseDoubleAttr("r", resistance);
 	}
 
 	Point ps3, ps4;
+
 	void setPoints() {
-	    super.setPoints();
-	    calcLeads(32);
-	    ps3 = new Point();
-	    ps4 = new Point();
+		super.setPoints();
+		calcLeads(32);
+		ps3 = new Point();
+		ps4 = new Point();
 	}
-	
+
 	void draw(Graphics g) {
-	    int segments = 16;
-	    int i;
-	    int ox = 0;
-	    //int hs = showEuroResistors() ? 6 : 8;
-	    int hs=6;
-	    double v1 = volts[0];
-	    double v2 = volts[1];
-	    setBbox(point1, point2, hs);
-	    draw2Leads(g);
-	    
-	    //   double segf = 1./segments;
-	    double len = distance(lead1, lead2);
-	    g.context.save();
-	    g.context.setLineWidth(3.0);
-	    g.context.transform(((double)(lead2.x-lead1.x))/len, ((double)(lead2.y-lead1.y))/len, -((double)(lead2.y-lead1.y))/len,((double)(lead2.x-lead1.x))/len,lead1.x,lead1.y);
-	    if (app.menus.voltsCheckItem.getState() ) {
-		CanvasGradient grad = g.context.createLinearGradient(0,0,len,0);
-		grad.addColorStop(0, getVoltageColor(g,v1).getHexValue());
-		grad.addColorStop(1.0, getVoltageColor(g,v2).getHexValue());
-		g.context.setStrokeStyle(grad);
-	    } else
-		setPowerColor(g, true);
-	    if (dn < 30)
-		hs = 2;
-	    if (!showEuroResistors()) {
-		g.context.beginPath();
-		g.context.moveTo(0,0);
-		for (i=0;i<4;i++){
-		    g.context.lineTo((1+4*i)*len/16, hs);
-		    g.context.lineTo((3+4*i)*len/16, -hs);
-		}
-		g.context.lineTo(len, 0);
-		g.context.stroke();
+		int segments = 16;
+		int i;
+		int ox = 0;
+		// int hs = showEuroResistors() ? 6 : 8;
+		int hs = 6;
+		double v1 = volts[0];
+		double v2 = volts[1];
+		setBbox(point1, point2, hs);
+		draw2Leads(g);
 
-	    } else {
-		g.context.strokeRect(0, -hs, len, 2.0*hs);
-	    }
-	    g.context.restore();
-	    if (showValues()) {
-		String s = getShortUnitText(resistance, "");
-		drawValues(g, s, hs+2);
-	    }
-	    doDots(g);
-	    drawPosts(g);
+		// double segf = 1./segments;
+		double len = distance(lead1, lead2);
+		g.context.save();
+		g.context.setLineWidth(3.0);
+		g.context.transform(((double) (lead2.x - lead1.x)) / len, ((double) (lead2.y - lead1.y)) / len,
+				-((double) (lead2.y - lead1.y)) / len, ((double) (lead2.x - lead1.x)) / len, lead1.x, lead1.y);
+		if (app.menus.voltsCheckItem.getState()) {
+			CanvasGradient grad = g.context.createLinearGradient(0, 0, len, 0);
+			grad.addColorStop(0, getVoltageColor(g, v1).getHexValue());
+			grad.addColorStop(1.0, getVoltageColor(g, v2).getHexValue());
+			g.context.setStrokeStyle(grad);
+		} else
+			setPowerColor(g, true);
+		if (dn < 30)
+			hs = 2;
+		if (!showEuroResistors()) {
+			g.context.beginPath();
+			g.context.moveTo(0, 0);
+			for (i = 0; i < 4; i++) {
+				g.context.lineTo((1 + 4 * i) * len / 16, hs);
+				g.context.lineTo((3 + 4 * i) * len / 16, -hs);
+			}
+			g.context.lineTo(len, 0);
+			g.context.stroke();
+
+		} else {
+			g.context.strokeRect(0, -hs, len, 2.0 * hs);
+		}
+		g.context.restore();
+		if (showValues()) {
+			String s = getShortUnitText(resistance, "");
+			drawValues(g, s, hs + 2);
+		}
+		doDots(g);
+		drawPosts(g);
 	}
 
-	void addRoutingObstacle(WireRouter wr) { addRoutingObstacleWithLeads(wr, 6); }
+	void addRoutingObstacle(WireRouter wr) {
+		addRoutingObstacleWithLeads(wr, 6);
+	}
 
 	void calculateCurrent() {
-	    current = (volts[0]-volts[1])/resistance;
-	    //System.out.print(this + " res current set to " + current + "\n");
+		current = (volts[0] - volts[1]) / resistance;
+		// System.out.print(this + " res current set to " + current + "\n");
 	}
+
 	void stamp() {
-	    sim.stampResistor(nodes[0], nodes[1], resistance);
+		sim.stampResistor(nodes[0], nodes[1], resistance);
 	}
+
 	void getInfo(String arr[]) {
-	    arr[0] = "resistor";
-	    getBasicInfo(arr);
-	    arr[3] = "R = " + getUnitText(resistance, Locale.ohmString);
-	    arr[4] = "P = " + getUnitText(getPower(), "W");
+		arr[0] = "resistor";
+		getBasicInfo(arr);
+		arr[3] = "R = " + getUnitText(resistance, Locale.ohmString);
+		arr[4] = "P = " + getUnitText(getPower(), "W");
 	}
-	@Override String getScopeText(int v) {
-	    return Locale.LS("resistor") + ", " + getUnitText(resistance, Locale.ohmString);
+
+	@Override
+	String getScopeText(int v) {
+		return Locale.LS("resistor") + ", " + getUnitText(resistance, Locale.ohmString);
 	}
+
 	public EditInfo getEditInfo(int n) {
-	    // ohmString doesn't work here on linux
-	    if (n == 0)
-		return new EditInfo("Resistance (ohms)", resistance, 0, 0);
-	    return null;
+		// ohmString doesn't work here on linux
+		if (n == 0)
+			return new EditInfo("Resistance (ohms)", resistance, 0, 0);
+		return null;
 	}
+
 	public void setEditValue(int n, EditInfo ei) {
-	    resistance = (ei.value <= 0) ? 1e-9 : ei.value;
+		resistance = (ei.value <= 0) ? 1e-9 : ei.value;
 	}
-	int getShortcut() { return 'r'; }
-	double getResistance() { return resistance; }
-	void setResistance(double r) { resistance = r; }
-    }
+
+	int getShortcut() {
+		return 'r';
+	}
+
+	double getResistance() {
+		return resistance;
+	}
+
+	void setResistance(double r) {
+		resistance = r;
+	}
+}
